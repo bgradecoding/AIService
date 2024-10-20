7c6a9555d8fcf8a7bde532ee1ab093c8001d694d
Jinho Jung <jinhojung@Jinhos-MacBook-Pro.local>
2024-10-21 00:36:58 +0900

    :art: (font) pretendard font 적용

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,15 @@
 import '@/res/css/globals.css';
 import { useTranslations } from 'next-intl';
+import localFont from 'next/font/local';
 import { NextIntlClientProvider, useMessages } from 'next-intl';
+
+const font = localFont({
+  src: '../../res/fonts/PretendardVariable.woff2',
+  weight: '400 900',
+  display: 'swap',
+  variable: '--font-pretendard',
+});
+
 export default function RootLayout({
   children,
   params: { locale },
@@ -18,7 +27,7 @@ export default function RootLayout({
       <meta name="keywords" content="" />
       <meta name="author" content="" />
       <title>{t('common.title')}</title>
-      <body className="">
+      <body className={`${font.variable} font-pretendard`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
